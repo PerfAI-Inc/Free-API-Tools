621c7435c70ca1095305c7d1d29d6f2c5c11c825
yahyaobeid <yobeid@sfsu.edu>
2024-02-05 18:52:07 -0800

    Added tracking

--- governance-ai/src/app.module.ts
+++ governance-ai/src/app.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GovernanceAiController } from './governance-ai/governance-ai.controller';
 import { GovernanceAiService } from './governance-ai/governance-ai.service';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { RequestTrackingMiddleware } from './middleware/request-tracking.middleware';
+
 
 @Module({
   imports: [],
   controllers: [AppController, GovernanceAiController],
   providers: [AppService, GovernanceAiService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestTrackingMiddleware)
+      .forRoutes('*');
+  }
+}
